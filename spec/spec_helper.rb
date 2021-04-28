05c70d28ecd28db749b95f81692a3f36fcdf7fa5
mxmCherry <mxmCherry@gmail.com>
2021-04-28 20:04:14 +0300

    hk: spec cosmetics - extract app to a (linkable) spec/scenario/ file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,7 @@
-# TODO: this looks weird, how to NOT mess with LOAD_PATH?
-$LOAD_PATH << './lib'
-require 'grape-apidoc'
+require_relative '../lib/grape-apidoc'
 
 require 'grape'
 require 'grape-entity'
 require 'stringio'
 
-module Mock
-  Foo = Struct.new(:foo_id)
-
-  class Foo::Entity < Grape::Entity
-    expose :foo_id, documentation: { type: 'Integer', desc: 'Foo ID' }
-  end
-
-  Bar = Struct.new(:bar_id, :foos)
-
-  class Bar::Entity < Grape::Entity
-    expose :bar_id, documentation: { type: 'Integer', desc: 'Bar ID' }
-    expose :foos,
-           using: Mock::Foo::Entity,
-           documentation: { type: 'Object', is_array: true, desc: 'Associated Foos' }
-  end
-end
+require_relative './scenario/mock_project'
